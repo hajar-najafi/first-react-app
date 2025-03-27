883cc3bc428fb0c48479f0ce8cab1764aa87281f
DevArchibuzz <git@archibuzz.com>
2025-03-27 09:21:33 +0100

    homepage and hompage for guest users created

---
+++ app/components/FlashMessages.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react"
+
+function FlashMessages(props) {
+  return (
+    <div className="floating-alerts">
+      {props.messages.map(msg => {
+        return <div className="alert alert-success text-center floating-alert shadow-sm">{msg}</div>
+      })}
+    </div>
+  )
+}
+
+export default FlashMessages
---
+++ app/main.js
@@ -0,0 +1,69 @@
+import React, { useState, useReducer } from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { useImmerReducer } from "use-immer"
+import Axios from "axios"
+Axios.defaults.baseURL = "http://localhost:8080"
+
+import StateContext from "./StateContext"
+import DispatchContext from "./DispatchContext"
+
+// My Components
+import Header from "./components/Header"
+import HomeGuest from "./components/HomeGuest"
+import Home from "./components/Home"
+import Footer from "./components/Footer"
+import About from "./components/About"
+import Terms from "./components/Terms"
+import CreatePost from "./components/CreatePost"
+import ViewSinglePost from "./components/ViewSinglePost"
+import FlashMessages from "./components/FlashMessages"
+
+function Main() {
+  const initialState = {
+    loggedIn: Boolean(localStorage.getItem("complexappToken")),
+    flashMessages: []
+  }
+
+  function ourReducer(draft, action) {
+    switch (action.type) {
+      case "login":
+        draft.loggedIn = true
+        return
+      case "logout":
+        draft.loggedIn = false
+        return
+      case "flashMessage":
+        draft.flashMessages.push(action.value)
+        return
+    }
+  }
+
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState)
+
+  return (
+    <StateContext.Provider value={state}>
+      <DispatchContext.Provider value={dispatch}>
+        <BrowserRouter>
+          <FlashMessages messages={state.flashMessages} />
+          <Header />
+          <Routes>
+            <Route path="/" element={state.loggedIn ? <Home /> : <HomeGuest />} />
+            <Route path="/post/:id" element={<ViewSinglePost />} />
+            <Route path="/create-post" element={<CreatePost />} />
+            <Route path="/about-us" element={<About />} />
+            <Route path="/terms" element={<Terms />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </DispatchContext.Provider>
+    </StateContext.Provider>
+  )
+}
+
+const root = ReactDOM.createRoot(document.querySelector("#app"))
+root.render(<Main />)
+
+if (module.hot) {
+  module.hot.accept()
+}
